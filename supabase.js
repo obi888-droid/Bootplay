5acb05b92a859a748360d185b976f0e8687e0a8a
obi888-droid <gamertoy249@gmail.com>
2026-09-17 13:36:59 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://nrratqzlmkvrkszlmyxk.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_x_nn5uNMnnlqK2rvxZFv8g_S6NXlJNy";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
